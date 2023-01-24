eda5b150c0279558da76c7945e27bd786a97495f
LarryHsiao <larryhsiao@larryhsiao.com>
2023-01-25 01:56:59 +0800

    Empty widget of resident page.

--- lib/chart_page.dart
+++ lib/chart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChartPageWidget extends StatefulWidget {
   const ChartPageWidget({Key? key}) : super(key: key);
--- lib/resident_page.dart
+++ lib/resident_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:user_meals/user_meals/entities/resident.dart';
 
 class ResidentPageWidget extends StatefulWidget {
   const ResidentPageWidget({Key? key}) : super(key: key);
@@ -8,10 +9,50 @@ class ResidentPageWidget extends StatefulWidget {
 }
 
 class _ResidentPageWidgetState extends State<ResidentPageWidget> {
+  final _residents = <Resident>[];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _bodyWidget(),
+    );
+  }
 
+  Widget _bodyWidget() {
+    if (_residents.isEmpty) {
+      return _emptyWidget();
+    } else {
+      return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: _residents.length,
+        itemBuilder: (context, idx) {
+          return Container(
+            child: Center(
+              child: Text("Text"),
+            ),
+          );
+        },
+      );
+    }
+  }
+
+  Widget _emptyWidget() {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("沒有住民資料"),
+            Text("點選\"+\"新增住民"),
+          ],
+        ),
+      ),
     );
   }
 }
