4f91b1247ba598f1e0912c0927bb984173264427
LarryHsiao <larryhsiao@larryhsiao.com>
2023-01-26 00:58:02 +0800

    Skeleton of create resident dialog.

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/widgets.dart';
 import 'package:user_meals/chart_page.dart';
 import 'package:user_meals/meal_page.dart';
 import 'package:user_meals/resident_page.dart';
+import 'package:user_meals/user_meals/create_resident.dart';
+import 'package:user_meals/utils_misc.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key, required this.title}) : super(key: key);
@@ -20,8 +22,10 @@ class _LandingPageState extends State<LandingPage> {
 
   var _currentPageIndex = indexPageUser;
 
-  void _incrementCounter() {
-    setState(() {});
+  void _onCreateResidentPressed() {
+    showCreateResidentDialog()
+        .then((value) => setState(() {}))
+        .onError((error, stackTrace) => null);
   }
 
   Widget _pageWidget() {
@@ -34,6 +38,60 @@ class _LandingPageState extends State<LandingPage> {
         /* case INDEX_PAGE_USER */
         return const ResidentPageWidget();
     }
+  }
+
+  Future<void> showCreateResidentDialog() async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          final nameController = TextEditingController();
+          final birthdayController = TextEditingController();
+          final ageController = TextEditingController();
+          return Dialog(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text("新增住民"),
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(labelText: "姓名"),
+                ),
+                TextField(
+                  controller: birthdayController,
+                  decoration: const InputDecoration(labelText: "生日"),
+                ),
+                TextField(
+                  controller: ageController,
+                  decoration: const InputDecoration(labelText: "年齡"),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("取消"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        Navigator.pop(context);
+                        CreateResident()
+                            .execute(nameController.value.text, 0)
+                            .then((value) => setState(() {}))
+                            .onError(
+                              (error, stackTrace) => UtilsMisc.onError(
+                                context,
+                                error.toString() + nameController.value.text,
+                              ),
+                            );
+                      },
+                      child: const Text("新增"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
   }
 
   @override
@@ -65,7 +123,7 @@ class _LandingPageState extends State<LandingPage> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _onCreateResidentPressed,
         tooltip: 'New resident',
         child: const Icon(Icons.add),
       ),
